9efb5e0bfa213af1035dc602baefd4c0dae89c96
Fei Liu <liu.feiwood@gmail.com>
2014-08-04 08:34:58 +0200

    rearrange array related

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
         },
         watch: {
             scripts: {
-                files: ['src/*.js', 'test/*.js'],
+                files: ['src/**/*.js', 'test/*.js'],
                 tasks: ['jasmine']
             }
         }
